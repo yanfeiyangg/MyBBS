9ad1a7ae8837bfa3061d3823d8c457e9eb9cfacc
yan <724028892@qq.com>
2018-11-11 21:58:34 +0800

    首页添加了Github、QQ、微信等链接

--- app01/static/js/index.js
+++ app01/static/js/index.js
@@ -1,7 +1,14 @@
-$("#backend").click(function () {
-    if (!"{{ request.user.username }}") {
-        location.href = "/login/";
-    } else {
-        location.href = "/blog/backend/";
-    }
-})
+$("#id_qq").hover(function () {
+    $("#QQCode").css('display','block');
+});
+
+$("#id_qq").mouseout(function () {
+    $("#QQCode").css('display','none');
+});
+$("#id_a_wechat").hover(function () {
+    $("#WeChatCode").css('display','block');
+});
+
+$("#id_a_wechat").mouseout(function () {
+    $("#WeChatCode").css('display','none');
+});
