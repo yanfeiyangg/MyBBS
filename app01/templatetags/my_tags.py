ae69ea417c305bc6a13308f41219ce505c205b64
yan <724028892@qq.com>
2019-03-22 11:38:02 +0800

    标签、分类过滤基于数量逆序排序，只显示文章数量前10的标签、分类

--- app01/templatetags/my_tags.py
+++ app01/templatetags/my_tags.py
@@ -12,13 +12,13 @@ def get_menu(username=None):
     if username == None:
         # ************   获取所有标签、分类、日期归档等 *********************
         # 找到博客下的所有分类以及对应的文章数量
-        cate_list = models.Category.objects.all().annotate(count=Count("article")).values_list("title", "count")
+        cate_list = models.Category.objects.all().annotate(count=Count("article")).order_by("-count").values_list("title", "count")[:10]
         # 找到博客下的所有标签以及对应的文章数量
-        tag_list = models.Tag.objects.all().annotate(count=Count("article")).values_list("title", "count")
+        tag_list = models.Tag.objects.all().annotate(count=Count("article")).order_by("-count").values_list("title", "count")[:10]
         # 日期归档
         date_list = models.Article.objects.all().extra(
             select={"create_ym": "DATE_FORMAT(create_time,'%%Y-%%m')"}).values("create_ym").annotate(
-            count=Count("*")).values_list("create_ym", "count")
+            count=Count("*")).order_by("-count").values_list("create_ym", "count")[:10]
     else:
         # 通过username找到对应的user对象
         print(username)
@@ -26,14 +26,13 @@ def get_menu(username=None):
         # 通过一对一关系，找到user对象的博客
         blog = user.blog
         # 找到博客下的所有分类以及对应的文章数量
-        cate_list = models.Category.objects.filter(blog=blog).annotate(count=Count("article")).values_list("title",
-                                                                                                           "count")  # 找到博客下的所有分类以及对应的文章数量
-        # 找到博客下的所有标签以及对应的文章数量
-        tag_list = models.Tag.objects.filter(blog=blog).annotate(count=Count("article")).values_list("title", "count")
+        cate_list = models.Category.objects.filter(blog=blog).annotate(count=Count("article")).distinct().order_by("-count").values_list("title","count")  # 找到博客下的所有分类以及对应的文章数量
+        #         # 找到博客下的所有标签以及对应的文章数量
+        tag_list = models.Tag.objects.filter(blog=blog).annotate(count=Count("article")).order_by("-count").values_list("title", "count")
         # 日期归档
         date_list = models.Article.objects.filter(user=user).extra(
             select={"create_ym": "DATE_FORMAT(create_time,'%%Y-%%m')"}).values("create_ym").annotate(
-            count=Count("*")).values_list("create_ym", "count")
+            count=Count("*")).order_by("-count").values_list("create_ym", "count")
     return locals()
 
 
